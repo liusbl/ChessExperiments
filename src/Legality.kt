0ad1a88ae47dbbd34970b91c2d10523433e6dc3a
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-09-03 23:17:18 +0300

    Create Legality class for representing legal/illegal positions

---
+++ src/Legality.kt
@@ -0,0 +1,13 @@
+sealed class Legality(val letterList: List<Char>) {
+    object Legal : Legality(listOf('L'))
+
+    sealed class Illegal(letterList: List<Char>) : Legality(letterList) {
+        class Combined(legalityList: List<Illegal>) : Illegal(legalityList.map(Illegal::letterList).flatten())
+
+        object KingsAdjacent : Illegal(listOf('K'))
+
+        object CheckButWrongMove : Illegal(listOf('C'))
+    }
+
+    override fun toString() = letterList.toString()
+}
