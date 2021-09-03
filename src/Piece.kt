b62a4046633899e58697d395f5796b4d403d44a4
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-09-04 00:13:27 +0300

    Add movement to Piece (for now all are Movement.None)

--- src/Piece.kt
+++ src/Piece.kt
@@ -1,19 +1,22 @@
-sealed class Piece(val letter: Char) {
+sealed class Piece(
+    val letter: Char,
+    val movement: Movement
+) {
     override fun toString() = letter.toString()
 
-    object Empty : Piece('-')
+    object Empty : Piece('-', Movement.None)
 
-    class King(color: Color) : Piece(if (color == Color.WHITE) 'K' else 'k')
+    class King(color: Color) : Piece(if (color == Color.WHITE) 'K' else 'k', Movement.None)
 
-    class Queen(color: Color) : Piece(if (color == Color.WHITE) 'Q' else 'q')
+    class Queen(color: Color) : Piece(if (color == Color.WHITE) 'Q' else 'q', Movement.None)
 
-    class Rook(color: Color) : Piece(if (color == Color.WHITE) 'R' else 'r')
+    class Rook(color: Color) : Piece(if (color == Color.WHITE) 'R' else 'r', Movement.None)
 
-    class Bishop(color: Color) : Piece(if (color == Color.WHITE) 'B' else 'b')
+    class Bishop(color: Color) : Piece(if (color == Color.WHITE) 'B' else 'b', Movement.None)
 
-    class Knight(color: Color) : Piece(if (color == Color.WHITE) 'N' else 'n')
+    class Knight(color: Color) : Piece(if (color == Color.WHITE) 'N' else 'n', Movement.None)
 
-    class Pawn(color: Color) : Piece(if (color == Color.WHITE) 'P' else 'p')
+    class Pawn(color: Color) : Piece(if (color == Color.WHITE) 'P' else 'p', Movement.None)
 
     enum class Color { WHITE, BLACK }
 }
