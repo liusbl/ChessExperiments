bbee2fba05671ab29720d0ef5bae4772c70291ea
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-09-25 00:53:16 +0300

    Check two kings legality

--- src/BoardFactory.kt
+++ src/BoardFactory.kt
@@ -1,3 +1,6 @@
+import kotlin.math.hypot
+import kotlin.math.sqrt
+
 object BoardFactory {
     fun createAllPieceBoards(size: Int, pieces: List<Piece>): List<Board> {
         val emptyBoard = Board.createEmpty(size)
@@ -25,20 +28,36 @@ object BoardFactory {
                         }
                     }
 
-                    val board1 = Board(size, updatedTileList, Move.WHITE, Legality.Legal, listOf())
-                    val a = board1.tileList.flatMap { tile ->
-                        tile.piece.movement.getNextBoardList(tile, board1)
-                    }
+                    // Check legality
+                    //  Two kings:
+                    val kings = updatedTileList.filter { tile -> tile.piece is Piece.King }
 
-                    val board2 = Board(size, updatedTileList, Move.BLACK, Legality.Legal, listOf())
-                    val b = board2.tileList.flatMap { tile ->
-                        tile.piece.movement.getNextBoardList(tile, board2)
-                    }
-
-                    listOf(
-                        board1.copy(nextBoardList = a),
-                        board2.copy(nextBoardList = b)
+                    val distance = if (kings.size < 2) 3.toDouble() else hypot(
+                        kings[1].location.x - kings[0].location.x.toDouble(),
+                        kings[1].location.y - kings[0].location.y.toDouble()
                     )
+
+                    if (distance < 2) {
+                        listOf(
+                            Board(size, updatedTileList, Move.WHITE, Legality.Illegal.KingsAdjacent, listOf()),
+                            Board(size, updatedTileList, Move.BLACK, Legality.Illegal.KingsAdjacent, listOf())
+                        )
+                    } else {
+                        val board1 = Board(size, updatedTileList, Move.WHITE, Legality.Legal, listOf())
+                        val a = board1.tileList.flatMap { tile ->
+                            tile.piece.movement.getNextBoardList(tile, board1)
+                        }
+
+                        val board2 = Board(size, updatedTileList, Move.BLACK, Legality.Legal, listOf())
+                        val b = board2.tileList.flatMap { tile ->
+                            tile.piece.movement.getNextBoardList(tile, board2)
+                        }
+
+                        listOf(
+                            board1.copy(nextBoardList = a),
+                            board2.copy(nextBoardList = b)
+                        )
+                    }
                 }
             }
         }
