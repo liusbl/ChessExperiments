af9bac20a04090ce1b84d43649bd5fefb87e453f
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-07 23:42:36 +0300

    Start generating FEN in BoardFenMapper.kt

--- src/BoardFenMapper.kt
+++ src/BoardFenMapper.kt
@@ -12,17 +12,28 @@
  * Order of custom FEN parts:
  *  0. Index
  *  1. Legality
- *  2. Check state
- *  3. Next Board indexes
+ *  2. Check state (unless Illegal)
+ *  3. Next Board indexes (unless Illegal)
  *
- * Sample FEN board with size 4:
- * 1k1K/4/4/Q3 w~45,L,_;15,78,155,611
+ * Sample of legal FEN board with size 4:
+ * 1k1K/4/4/Q3 w~45,L,_;15,78,155,611 (numbers here are random)
  *
  * Which represents the board:
  * -k-K
  * ----
  * ----
  * Q---
+ * White to move
+ *
+ *
+ * Sample of illegal FEN board with size 4:
+ * kKQ1/4/4/4 w~45,K
+ *
+ * Which represents the board:
+ * kKQ-
+ * ----
+ * ----
+ * ----
  * White to move
  *
  * -------
@@ -44,13 +55,52 @@
  * 2. Side that is making a check has the next move - C
  *
  */
-
 object BoardFenMapper {
-    fun getFen(board: Board): String {
-        return TODO()
+    fun getFen(board: Board.Final): String {
+        var result = ""
+        board.tileList.chunked(board.size)
+            .forEach { row ->
+                var emptyCount = 0
+                row.forEach { tile ->
+                    if (tile.piece is Piece.Empty) {
+                        emptyCount++
+                    } else {
+                        result += if (emptyCount == 0) {
+                            "${tile.piece}"
+                        } else {
+                            "${emptyCount}${tile.piece}".also {
+                                emptyCount = 0
+                            }
+                        }
+                    }
+                }
+                result += if (emptyCount != 0) {
+                    "$emptyCount/"
+                } else {
+                    "/"
+                }
+            }
+        result = result.dropLast(1)
+//            result += " ${move.letter}"
+//        result += " $legality"
+//            result += " $nextBoardList"
+        return result
     }
 
     fun getBoard(fen: String): Board {
         return TODO()
     }
+}
+
+// For testing purposes
+fun main() {
+//    val emptyBoard = createEmptyBoard(4)
+//    val tileList = emptyBoard.tileList.toMutableList().run {
+//        set(1, get(1).copy(piece = Piece.King(Piece.Color.BLACK)))
+//        set(3, get(3).copy(piece = Piece.King(Piece.Color.WHITE)))
+//        set(12, get(12).copy(piece = Piece.Queen(Piece.Color.WHITE)))
+//    }
+//
+//
+//    BoardFenMapper.getFen()
 }
