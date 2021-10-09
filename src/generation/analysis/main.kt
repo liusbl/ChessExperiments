474752788f6c3e6494bc8cc9f8ea05b4b8690251
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-09 22:16:24 +0300

    Create reading and parsing for FEN list main.kt

--- src/generation/analysis/main.kt
+++ src/generation/analysis/main.kt
@@ -1,5 +1,15 @@
 package generation.analysis
 
-fun main() {
+import generation.BoardFenMapper
+import java.io.File
+import java.time.LocalDateTime
 
+fun main() {
+    println("Step #0: Read file. ${LocalDateTime.now()}")
+    val fileLines = File("out_fen_8.txt").readLines()
+
+    println("Step #1: Parse fen information to board list. ${LocalDateTime.now()}")
+    val indexBoardList = fileLines.map(BoardFenMapper::getBoard)
+
+    println("Finished ${LocalDateTime.now()}")
 }
