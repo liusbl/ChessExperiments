12f9949d55ea535e3830caf71d398d0983b0a780
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-09 20:47:46 +0300

    Add 2 king drawn games

--- src/generation/BoardFactory.kt
+++ src/generation/BoardFactory.kt
@@ -24,25 +24,38 @@ fun main() {
     println("Step #2: Combine all single piece boards. ${LocalDateTime.now()}")
     val combinedPieceBoardList = combineSinglePieceBoardLists(singlePieceBoardList)
 
-    println("Step #3: Append moves to boards. ${LocalDateTime.now()}")
-    val allCombinedPieceBoardListWithMoves = appendMovesToBoardList(combinedPieceBoardList)
 
-    println("Step #4: Append legalities to boards. ${LocalDateTime.now()}")
+    println("Step #3: Create boards with only kings. ${LocalDateTime.now()}")
+    val kingBoardList = createAllSinglePieceBoardList(
+        emptyBoard,
+        listOf(
+            King(Color.WHITE),
+            King(Color.BLACK),
+        )
+    )
+
+    println("Step #4: Combine board with only kings. ${LocalDateTime.now()}")
+    val combinedKingBoardList = combineSinglePieceBoardLists(kingBoardList)
+
+    println("Step #5: Append moves to boards. ${LocalDateTime.now()}")
+    val allCombinedPieceBoardListWithMoves = appendMovesToBoardList(combinedPieceBoardList + combinedKingBoardList)
+
+    println("Step #6: Append legalities to boards. ${LocalDateTime.now()}")
     val boardListWithCheckState = appendCheckStateToBoardList(allCombinedPieceBoardListWithMoves)
 
-    println("Step #5: Append next board lists. ${LocalDateTime.now()}")
+    println("Step #7: Append next board lists. ${LocalDateTime.now()}")
     val boardListWithNextBoardLists = appendNextBoardLists(boardListWithCheckState)
 
-    println("Step #6: Filter legal next board lists. ${LocalDateTime.now()}")
+    println("Step #8: Filter legal next board lists. ${LocalDateTime.now()}")
     val boardListWithLegalNextBoardLists = filterLegalNextBoardLists(boardListWithNextBoardLists)
 
-    println("Step #7: Filter legal next board lists. ${LocalDateTime.now()}")
+    println("Step #9: Filter legal next board lists. ${LocalDateTime.now()}")
     val boardListWithNextIndexes = appendNextBoardIndexes(boardListWithLegalNextBoardLists)
 
-    println("Step #8: Create FEN representations. ${LocalDateTime.now()}")
+    println("Step #10: Create FEN representations. ${LocalDateTime.now()}")
     val boardFenList = boardListWithNextIndexes.map(BoardFenMapper::getFen)
 
-    println("Step #9: Print results to file. ${LocalDateTime.now()}")
+    println("Step #11: Print results to files. ${LocalDateTime.now()}")
     File("out_boards.txt").writeText(boardListWithLegalNextBoardLists.joinToString(separator = "\n"))
     File("out_fen.txt").writeText(boardFenList.joinToString(separator = "\n"))
 
