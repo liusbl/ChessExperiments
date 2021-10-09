cfaaf2772e14fbb1124ac764ba049782d7ba90bd
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-09 21:37:52 +0300

    Fix description of Step#9

--- src/generation/BoardFactory.kt
+++ src/generation/BoardFactory.kt
@@ -4,7 +4,7 @@ import generation.models.Piece.*
 import java.io.File
 import java.time.LocalDateTime
 
-private const val BOARD_SIZE = 3
+private const val BOARD_SIZE = 8
 
 // TODO Consider possible statistics
 fun main() {
@@ -49,14 +49,14 @@ fun main() {
     println("Step #8: Filter legal next board lists. ${LocalDateTime.now()}")
     val boardListWithLegalNextBoardLists = filterLegalNextBoardLists(boardListWithNextBoardLists)
 
-    println("Step #9: Filter legal next board lists. ${LocalDateTime.now()}")
+    println("Step #9: Append next board indexes. ${LocalDateTime.now()}")
     val boardListWithNextIndexes = appendNextBoardIndexes(boardListWithLegalNextBoardLists)
 
     println("Step #10: Create FEN representations. ${LocalDateTime.now()}")
     val boardFenList = boardListWithNextIndexes.map(BoardFenMapper::getFen)
 
     println("Step #11: Print results to files. ${LocalDateTime.now()}")
-    File("out_boards.txt").writeText(boardListWithNextIndexes.joinToString(separator = "\n"))
+//    File("out_boards.txt").writeText(boardListWithNextIndexes.joinToString(separator = "\n"))
     File("out_fen.txt").writeText(boardFenList.joinToString(separator = "\n"))
 
     println("Finished ${LocalDateTime.now()}")
