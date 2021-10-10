25b1e7093885698069ae5f594a22b1fbb7474157
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-10 09:54:22 +0300

    Add Move and CheckState to IndexGraph

--- src/generation/analysis/createIndexGraphList.kt
+++ src/generation/analysis/createIndexGraphList.kt
@@ -5,7 +5,7 @@ import generation.models.IndexGraph
 
 fun createIndexGraphList(indexBoardList: List<IndexBoard>) {
     val graphList = indexBoardList.map { board ->
-        IndexGraph(board.index, board.nextBoardIndexList, null)
+        IndexGraph(board.index, board.move, board.checkState, board.nextBoardIndexList, null)
     }
     graphList.forEach { graph ->
         graph.nextGraphList = graph.nextIndexList.mapNotNull { index -> graphList.getOrNull(index) }
--- src/generation/models/IndexGraph.kt
+++ src/generation/models/IndexGraph.kt
@@ -2,6 +2,8 @@ package generation.models
 
 class IndexGraph(
     val index: Int,
+    val move: Move,
+    val checkState: CheckState,
     val nextIndexList: List<Int>,
     var nextGraphList: List<IndexGraph>?
 )
