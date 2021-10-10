b8bbb0325b9dfe5a3d8996d6bf2f3242cffe58ee
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-10-10 23:17:47 +0300

    Add named constructor to IndexGraph.kt

--- src/generation/models/IndexGraph.kt
+++ src/generation/models/IndexGraph.kt
@@ -9,3 +9,14 @@ data class IndexGraph(
     val parentIndexList: MutableList<Int>,
     var nextGraphList: List<IndexGraph>?
 )
+
+@SuppressWarnings("FunctionName")
+fun IndexGraph(board: IndexBoard) = IndexGraph(
+    index = board.index,
+    isLegal = board.isLegal,
+    move = board.move,
+    checkState = board.checkState,
+    nextIndexList = board.nextBoardIndexList,
+    parentIndexList = mutableListOf(),
+    nextGraphList = null
+)
