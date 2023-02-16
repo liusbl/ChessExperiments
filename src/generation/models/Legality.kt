a3c2b5e9f1a41b5f694fb4bb92a3edb9258eacd9
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-02-16 23:15:59 +0200

    Add Legality#Unreachable, for now without it's implementation

--- src/generation/models/Legality.kt
+++ src/generation/models/Legality.kt
@@ -9,6 +9,20 @@ sealed class Legality(val letterList: List<Char>) {
         object KingsAdjacent : Illegal(listOf('K'))
 
         object CheckButWrongMove : Illegal(listOf('C'))
+
+        /**
+         * Happens when the state is technically possible, but not reachable from any other board state.
+         * Consider the following board state:
+         *  KQ1/2k/3, Black to move:
+         *
+         *  K Q -
+         *  - k -
+         *  - - -
+         *
+         * In this situation, there is no previous place for white's queen to have been to make this check.
+         * In practice this legality can simply be made by checking whether it has any parent board states.
+         */
+        object Unreachable : Illegal(listOf('U'))
     }
 
     override fun toString() = letterList.toString()
