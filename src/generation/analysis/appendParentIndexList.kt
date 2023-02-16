0e8222a4b0ab4c703cf4385de6560e3472e44b16
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-02-16 23:06:46 +0200

    Fix error in appendParentIndexList.kt where the index of the actual graph should be used, instead of it's list index

--- src/generation/analysis/appendParentIndexList.kt
+++ src/generation/analysis/appendParentIndexList.kt
@@ -5,7 +5,7 @@ import generation.models.IndexGraph
 fun appendParentIndexList(graphList: List<IndexGraph>) {
     graphList.forEach { graph ->
         graph.nextIndexList.forEach { nextIndex ->
-            graphList[nextIndex].parentIndexList.add(graph.index)
+            graphList.find { it.index == nextIndex }?.parentIndexList?.add(graph.index)
         }
     }
 }
