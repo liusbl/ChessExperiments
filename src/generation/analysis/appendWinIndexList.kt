a4a87f01615df7f018cace4b398f1e5a055dc829
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-02-17 00:29:40 +0200

    Add WinIndex=2 for black position with a single losing move, in appendWinIndexList.kt

--- src/generation/analysis/appendWinIndexList.kt
+++ src/generation/analysis/appendWinIndexList.kt
@@ -4,6 +4,7 @@ import generation.models.CheckState
 import generation.models.IndexGraph
 
 fun appendWinIndexList(graphList: List<IndexGraph>) {
+    // White to move
     graphList.forEach { graph ->
          graph.nextIndexList.forEach loop@{ nextIndex ->
             val nextGraph = graphList.find { it.index == nextIndex } ?: return@loop
@@ -12,6 +13,17 @@ fun appendWinIndexList(graphList: List<IndexGraph>) {
             }
         }
     }
-    // TODO iterate again and treat the win condition not as checkmate but as WinIndex = 1. Then WindIndex = 2.
-    //  Keep increasing until no more WinIndexes show up.
+
+    // Black to move
+    // "Stupid" simple approach - if black has a single move and it leads to WinIndex=1, then treat it as "winIndex=2".
+    // TODO Next perhaps check multiple
+    graphList.forEach { graph ->
+        if (graph.nextIndexList.size == 1) {
+            val nextIndex = graph.nextIndexList[0]
+            val nextGraph = graphList.find { it.index == nextIndex } ?: return@forEach
+            if (nextGraph.winIndexList.size == 1 && nextGraph.winIndexList[0].pliesUntilCheckmate == 1) {
+                graph.winIndexList.add(IndexGraph.WinIndex(nextIndex = nextIndex, pliesUntilCheckmate = 2))
+            }
+        }
+    }
 }
