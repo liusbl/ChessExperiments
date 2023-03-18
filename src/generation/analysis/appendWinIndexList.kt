b32323a435391031475293c75a7e38f8a407d9c6
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-03-18 22:43:13 +0200

    Filter white or black moves when necessary in appendWinIndexList.kt

--- src/generation/analysis/appendWinIndexList.kt
+++ src/generation/analysis/appendWinIndexList.kt
@@ -64,63 +64,65 @@ private fun appendImmediateDraws(graphList: List<IndexGraph>) {
 // "Stupid" simple approach - if black has a single move and it leads to WinIndex=1, then treat it as "winIndex=2".
 // TODO Next perhaps check multiple
 private fun handleBlackMoves(graphList: List<IndexGraph>) {
-    graphList.forEach { graph ->
-        if (graph.winIndexList.isNotEmpty()) return@forEach
-        if (graph.nextIndexList.isEmpty()) return@forEach
+    graphList.filter { it.move == Move.BLACK }
+        .forEach { graph ->
+            if (graph.winIndexList.isNotEmpty()) return@forEach
+            if (graph.nextIndexList.isEmpty()) return@forEach
 
-        val allMovesLose = graph.nextIndexList.all { nextIndex ->
-            val nextGraph = graphList.find { it.index == nextIndex }
-                ?: return@all false // For now this null checking works, because this only happens when King takes Queen and draws.
+            val allMovesLose = graph.nextIndexList.all { nextIndex ->
+                val nextGraph = graphList.find { it.index == nextIndex }
+                    ?: return@all false // For now this null checking works, because this only happens when King takes Queen and draws.
 //TODO                 ?: throw Exception("Unexpected index: $nextIndex, possibleIndexes: ${graphList.map { it.index }}")
 // This problem happens when a black King takes the Queen.
 
-            nextGraph.winIndexList.isNotEmpty()
-        }
-        if (allMovesLose) {
-            // TODO not sure about this. Is taking the max until checkmate really the right approach? Maybe?
-            val maxPliesUntilCheckmate = graph.nextIndexList.map { nextIndex ->
-                val nextGraph = graphList.find { it.index == nextIndex }
-                nextGraph?.winIndexList?.map { it.pliesUntilCheckmate } ?: listOf(Int.MIN_VALUE)// TODO
-            }.flatten().max()
-            graph.nextIndexList.all { nextIndex ->
-                graph.winIndexList.add(
-                    IndexGraph.WinIndex(
-                        nextIndex = nextIndex,
-                        pliesUntilCheckmate = maxPliesUntilCheckmate + 1
+                nextGraph.winIndexList.isNotEmpty()
+            }
+            if (allMovesLose) {
+                // TODO not sure about this. Is taking the max until checkmate really the right approach? Maybe?
+                val maxPliesUntilCheckmate = graph.nextIndexList.map { nextIndex ->
+                    val nextGraph = graphList.find { it.index == nextIndex }
+                    nextGraph?.winIndexList?.map { it.pliesUntilCheckmate } ?: listOf(Int.MIN_VALUE)// TODO
+                }.flatten().max()
+                graph.nextIndexList.all { nextIndex ->
+                    graph.winIndexList.add(
+                        IndexGraph.WinIndex(
+                            nextIndex = nextIndex,
+                            pliesUntilCheckmate = maxPliesUntilCheckmate + 1
+                        )
                     )
-                )
+                }
             }
         }
-    }
 }
 
 private fun handleWhiteMoves(graphList: List<IndexGraph>) {
-    graphList.forEach { graph ->
-        if (graph.winIndexList.isNotEmpty()) return@forEach
-        if (graph.nextIndexList.isEmpty()) return@forEach
+    graphList.filter { it.move == Move.WHITE }
+        .forEach { graph ->
+            if (graph.winIndexList.isNotEmpty()) return@forEach
+            if (graph.nextIndexList.isEmpty()) return@forEach
 
-        val anyMoveWins = graph.nextIndexList.any { nextIndex ->
-            val nextGraph = graphList.find { it.index == nextIndex }
-                ?: return@any false // For now this null checking works, because this only happens when King takes Queen and draws.
+            val anyMoveWins = graph.nextIndexList.any { nextIndex ->
+                val nextGraph = graphList.find { it.index == nextIndex }
+                    ?: return@any false // For now this null checking works, because this only happens when King takes Queen and draws.
 //TODO                 ?: throw Exception("Unexpected index: $nextIndex, possibleIndexes: ${graphList.map { it.index }}")
 // This problem happens when a black King takes the Queen.
-            nextGraph.winIndexList.isNotEmpty()
-        }
+                nextGraph.winIndexList.isNotEmpty()
+            }
 
-        if (anyMoveWins) {
-            // TODO not sure about this. Is taking the max until checkmate really the right approach? Maybe?
-            val minPliesUntilCheckmate = graph.nextIndexList.map { nextIndex ->
-                val nextGraph = graphList.find { it.index == nextIndex }
-                nextGraph?.winIndexList?.map { it.pliesUntilCheckmate } ?: listOf(Int.MAX_VALUE)// TODO
-            }.flatten().min()
-            graph.nextIndexList.all { nextIndex ->
-                graph.winIndexList.add(
-                    IndexGraph.WinIndex(
-                        nextIndex = nextIndex,
-                        pliesUntilCheckmate = minPliesUntilCheckmate + 1
+            if (anyMoveWins) {
+                // TODO not sure about this. Is taking the max until checkmate really the right approach? Maybe?
+                val minPliesUntilCheckmate = graph.nextIndexList.map { nextIndex ->
+                    val nextGraph = graphList.find { it.index == nextIndex }
+                    nextGraph?.winIndexList?.map { it.pliesUntilCheckmate } ?: listOf(Int.MAX_VALUE)// TODO
+                }.flatten().min()
+                graph.nextIndexList.all { nextIndex ->
+                    graph.winIndexList.add(
+                        IndexGraph.WinIndex(
+                            nextIndex = nextIndex,
+                            pliesUntilCheckmate = minPliesUntilCheckmate + 1
+                        )
                     )
-                )
+                }
             }
         }
-    }
 }
