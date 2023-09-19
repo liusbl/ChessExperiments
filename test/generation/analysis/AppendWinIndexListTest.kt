9f2e265ab9ec354d91c415dde43193d81dbae143
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-09-19 21:57:51 +0300

    Create empty test AppendWinIndexListTest.kt

---
+++ test/generation/analysis/AppendWinIndexListTest.kt
@@ -0,0 +1,11 @@
+package generation.analysis
+
+import org.junit.jupiter.api.Test
+
+class AppendWinIndexListTest {
+    @Test
+    fun appendWinIndexList() {
+        // TODO
+        appendWinIndexList(listOf())
+    }
+}
