7069daecf21b1249a3bc597ec532b3fb402bf67c
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-09-20 21:26:36 +0300

    Mock simple graph in AppendWinIndexListTest.kt

--- test/generation/analysis/AppendWinIndexListTest.kt
+++ test/generation/analysis/AppendWinIndexListTest.kt
@@ -1,11 +1,46 @@
 package generation.analysis
 
+import generation.models.CheckState
+import generation.models.IndexGraph
+import generation.models.Move
 import org.junit.jupiter.api.Test
 
 class AppendWinIndexListTest {
     @Test
     fun appendWinIndexList() {
-        // TODO
-        appendWinIndexList(listOf())
+        val list = listOf(
+            IndexGraph(
+                index = 0,
+                move = Move.WHITE,
+                checkState = CheckState.NONE,
+                nextIndexList = listOf(1),
+            ),
+            IndexGraph(
+                index = 1,
+                move = Move.BLACK,
+                checkState = CheckState.BLACK_IN_CHECKMATE,
+                nextIndexList = emptyList(),
+            )
+        )
+        appendWinIndexList(list)
+        println(list)
     }
+
+    private fun IndexGraph(
+        index: Int,
+        move: Move,
+        checkState: CheckState,
+        nextIndexList: List<Int>
+    ): IndexGraph = IndexGraph(
+        index = index,
+        usualFen = "", // Doesn't matter
+        fullFen = "", // Doesn't matter
+        isLegal = true,
+        move = move,
+        checkState = checkState,
+        parentIndexList = mutableListOf(), // Doesn't matter
+        nextIndexList = nextIndexList,
+        nextGraphList = mutableListOf(), // Doesn't matter
+        winIndexList = mutableListOf(), // Doesn't matter
+    )
 }
