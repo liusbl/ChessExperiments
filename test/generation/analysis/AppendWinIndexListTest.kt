9c2298cf3583de4a03bef53387dd4719190352d4
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-09-20 21:45:13 +0300

    Create proper single and two move mate tests in AppendWinIndexListTest.kt

--- test/generation/analysis/AppendWinIndexListTest.kt
+++ test/generation/analysis/AppendWinIndexListTest.kt
@@ -2,12 +2,15 @@ package generation.analysis
 
 import generation.models.CheckState
 import generation.models.IndexGraph
+import generation.models.IndexGraph.WinIndex
 import generation.models.Move
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class AppendWinIndexListTest {
+    // 0W -> 1B#
     @Test
-    fun appendWinIndexList() {
+    fun singleMoveMate() {
         val list = listOf(
             IndexGraph(
                 index = 0,
@@ -23,7 +26,42 @@ class AppendWinIndexListTest {
             )
         )
         appendWinIndexList(list)
-        println(list)
+
+        assertEquals(WinIndex(1, 1), list[0].winIndexList[0])
+    }
+
+    // 0W -> 1B -> 2W -> 3B#
+    @Test
+    fun twoMoveMate() {
+        val list = listOf(
+            IndexGraph(
+                index = 0,
+                move = Move.WHITE,
+                checkState = CheckState.NONE,
+                nextIndexList = listOf(1),
+            ),
+            IndexGraph(
+                index = 1,
+                move = Move.BLACK,
+                checkState = CheckState.NONE,
+                nextIndexList = listOf(2),
+            ),
+            IndexGraph(
+                index = 2,
+                move = Move.WHITE,
+                checkState = CheckState.NONE,
+                nextIndexList = listOf(3),
+            ),
+            IndexGraph(
+                index = 3,
+                move = Move.BLACK,
+                checkState = CheckState.BLACK_IN_CHECKMATE,
+                nextIndexList = emptyList(),
+            )
+        )
+        appendWinIndexList(list)
+
+        assertEquals(WinIndex(1, 3), list[0].winIndexList[0])
     }
 
     private fun IndexGraph(
